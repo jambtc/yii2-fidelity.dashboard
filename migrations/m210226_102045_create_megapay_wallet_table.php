e3f959f1105751aaad64f352c63df919a5610d59
Sergio Casizzone <jambtc@gmail.com>
2021-04-22 18:29:04 +0200

    fix wallet to wallets

--- migrations/m210226_102045_create_megapay_wallet_table.php
+++ migrations/m210226_102045_create_megapay_wallet_table.php
@@ -15,7 +15,7 @@ class m210226_102045_create_megapay_wallet_table extends Migration
      */
     public function safeUp()
     {
-        $this->createTable('{{%mp_wallet}}', [
+        $this->createTable('{{%mp_wallets}}', [
             'id' => $this->primaryKey(),
             'id_user' => $this->integer(11),
             'wallet_address' => $this->string(50)->notNull(),
@@ -24,15 +24,15 @@ class m210226_102045_create_megapay_wallet_table extends Migration
 
         // creates index for column `id_user`
         $this->createIndex(
-            '{{%idx-mp_wallet-id_user}}',
-            '{{%mp_wallet}}',
+            '{{%idx-mp_wallets-id_user}}',
+            '{{%mp_wallets}}',
             'id_user'
         );
 
-        //add foreign key for table `{{%mp_wallet}}`
+        //add foreign key for table `{{%mp_wallets}}`
         $this->addForeignKey(
             '{{%fk-megapay_wallet-user_id}}',
-            '{{%mp_wallet}}',
+            '{{%mp_wallets}}',
             'id_user',
             '{{%mp_users}}',
             'id',
@@ -53,10 +53,10 @@ class m210226_102045_create_megapay_wallet_table extends Migration
 
         // drops index for column `user_id`
         $this->dropIndex(
-            '{{%idx-mp_wallet-id_user}}',
+            '{{%idx-mp_wallets-id_user}}',
             '{{%mp_wallet}}'
         );
 
-        $this->dropTable('{{%mp_wallet}}');
+        $this->dropTable('{{%mp_wallets}}');
     }
 }
